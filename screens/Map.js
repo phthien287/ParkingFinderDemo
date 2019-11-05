7e082a94792c228ccc6cb2944af06e5fbeca084e
PHThien <phthien287@gmail.com>
2019-11-05 13:10:54 +0700

    add theme.js and modify header

--- screens/Map.js
+++ screens/Map.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import * as theme from "../theme";
 
 export default class Map extends React.Component {
   state = {
@@ -18,7 +19,17 @@ export default class Map extends React.Component {
   renderHeader() {
     return (
       <View style={styles.header}>
-        <Text> Header </Text>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerTitle}>Detected Location</Text>
+          <Text style={styles.headerLocation}>San Francisco, US</Text>
+        </View>
+        <View style={styles.headerIcon}>
+          <Ionicons
+            name={"md-menu"}
+            size={theme.SIZE.icon * 1.5}
+            color={theme.COLOR.gray}
+          ></Ionicons>
+        </View>
       </View>
     );
   }
@@ -27,75 +38,42 @@ export default class Map extends React.Component {
     const { hours } = this.state;
     return (
       <View key={`parking -${item.id}`} style={styles.parking}>
-        <View style={{ flex: 1, flexDirection: "column" }}>
-          <Text style={{ fontSize: 16 }}>
+        <View style={styles.hours}>
+          <Text style={styles.title}>
             x {item.spots} {item.title}
           </Text>
-          <View
-            style={{
-              width: 100,
-              borderRadius: 6,
-              borderColor: "grey",
-              borderWidth: 0.7,
-              padding: 4
-            }}
-          >
-            <Text style={{ fontSize: 16 }}>5:00 hrs</Text>
+          <View style={styles.hoursBorder}>
+            <Text style={styles.title}>5:00 hrs</Text>
           </View>
         </View>
-        <View style={{ flex: 1.5, flexDirection: "row" }}>
-          <View
-            style={{
-              flex: 0.5,
-              justifyContent: "center",
-              marginHorizontal: 24
-            }}
-          >
-            <View
-              style={{
-                flex: 1,
-                justifyContent: "space-between",
-                alignItems: "center",
-                flexDirection: "row"
-              }}
-            >
-              <Ionicons name="md-pricetag" size={16} color="#70818A"></Ionicons>
+        <View style={styles.parkingInfoContainer}>
+          <View style={styles.parkingInfo}>
+            <View style={styles.parkingInfoIcon}>
+              <Ionicons
+                name="md-pricetag"
+                size={theme.SIZE.icon}
+                color={theme.COLOR.gray}
+              ></Ionicons>
               <Text> ${item.price}</Text>
             </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: "space-between",
-                alignItems: "center",
-                flexDirection: "row"
-              }}
-            >
-              <Ionicons name="md-star" size={16} color="#70818A"></Ionicons>
+            <View style={styles.parkingInfoIcon}>
+              <Ionicons
+                name="md-star"
+                size={theme.SIZE.icon}
+                color={theme.COLOR.gray}
+              ></Ionicons>
               <Text> {item.rating}</Text>
             </View>
           </View>
           <TouchableOpacity style={styles.buy} activeOpacity={0.6}>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: "center"
-              }}
-            >
-              <Text style={{ fontSize: 25, color: "white" }}>
-                ${item.price * 2}
-              </Text>
-              <Text style={{ color: "white" }}>
+            <View style={styles.buyTotal}>
+              <Text style={styles.buyButtonText}>${item.price * 2}</Text>
+              <Text style={{ color: theme.COLOR.white }}>
                 {item.price}x{hours[item.id]} hrs
               </Text>
             </View>
-            <View
-              style={{
-                flex: 0.5,
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
-              <Text style={{ fontSize: 25, color: "white" }}>></Text>
+            <View style={styles.buyButton}>
+              <Text style={styles.buyButtonText}>></Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -163,53 +141,60 @@ export default class Map extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  header: {
+    flex: 0.5,
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingHorizontal: theme.SIZE.base * 2,
+    paddingTop: theme.SIZE.base * 2.5,
+    paddingBottom: theme.SIZE.base * 1.5
+  },
+  headerContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  headerTitle: {
+    color: theme.COLOR.gray
+  },
+  headerLocation: {
+    fontSize: theme.SIZE.font,
+    fontWeight: "500",
+    paddingVertical: theme.SIZE.base / 3
+  },
+  headerIcon: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-end"
+  },
+
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.COLOR.white,
     alignItems: "center",
     justifyContent: "center"
+  },
+  marker: {
+    flexDirection: "row",
+    borderColor: theme.COLOR.red,
+    borderRadius: theme.SIZE.base * 2,
+    paddingVertical: theme.SIZE.base,
+    paddingHorizontal: theme.SIZE.base * 2,
+    borderWidth: 1
   },
   map: {
     flex: 3,
     width: Dimensions.get("window").width
   },
-  header: {
-    flex: 0.5,
-    justifyContent: "center"
-  },
-  parking: {
-    flexDirection: "row",
-    backgroundColor: "white",
-    borderRadius: 6,
-    padding: 15,
-    marginHorizontal: 24,
-    width: Dimensions.get("window").width - 48
-  },
-  parkings: {
-    // alignContent: "flex-end",
-    flex: 1,
-    position: "absolute",
-    right: 0,
-    left: 0,
-    bottom: 24
-  },
-  buy: {
-    flex: 1.25,
-    backgroundColor: "#D83C54",
-    borderRadius: 6,
-    padding: 8,
-    flexDirection: "row"
-  },
   marker: {
     flexDirection: "row",
-    borderColor: "red",
-    borderRadius: 24,
-    paddingVertical: 12,
-    paddingHorizontal: 24,
+    borderColor: theme.COLOR.red,
+    borderRadius: theme.SIZE.base * 2,
+    paddingVertical: theme.SIZE.base,
+    paddingHorizontal: theme.SIZE.base * 2,
     borderWidth: 1
   },
   markerShadow: {
-    shadowColor: "#3D4448",
+    shadowColor: theme.COLOR.black,
     shadowOffset: {
       width: 0,
       height: 6
@@ -218,8 +203,64 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5
   },
-  markerPrice: { color: "#D83C54", fontWeight: "bold" },
-  markerStatus: { color: "#7D818A" }
+  markerPrice: { color: theme.COLOR.red, fontWeight: "bold" },
+  markerStatus: { color: theme.COLOR.gray },
+  parkings: {
+    flex: 1,
+    position: "absolute",
+    right: 0,
+    left: 0,
+    bottom: theme.SIZE.base * 2,
+    paddingBottom: theme.SIZE.base * 2
+  },
+  parking: {
+    flexDirection: "row",
+    backgroundColor: theme.COLOR.white,
+    borderRadius: 6,
+    padding: 15,
+    marginHorizontal: theme.SIZE.base * 2,
+    width: Dimensions.get("window").width - theme.SIZE.base * 4
+  },
+
+  buy: {
+    flex: 1.25,
+    backgroundColor: theme.COLOR.red,
+    borderRadius: 6,
+    padding: 8,
+    flexDirection: "row"
+  },
+
+  hours: { flex: 1, flexDirection: "column" },
+  title: { fontSize: theme.SIZE.font },
+  hoursBorder: {
+    width: 100,
+    borderRadius: 6,
+    borderColor: theme.COLOR.gray,
+    borderWidth: 0.7,
+    padding: 4
+  },
+  parkingInfoContainer: { flex: 1.5, flexDirection: "row" },
+  parkingInfo: {
+    flex: 0.5,
+    justifyContent: "center",
+    marginHorizontal: theme.SIZE.base * 2
+  },
+  parkingInfoIcon: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  buyTotal: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  buyButton: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buyButtonText: { fontSize: theme.SIZE.title, color: theme.COLOR.white }
 });
 
 const parkingsSpots = [
---
+++ theme.js
@@ -0,0 +1,16 @@
+const COLOR = {
+  red: "#D83C54",
+  gray: "#7D818A",
+  black: "#3D4448",
+  white: "#FFFFFF",
+  overlay: "#C1BEC0"
+};
+
+const SIZE = {
+  base: 12,
+  icon: 16,
+  font: 16,
+  title: 25
+};
+
+export { COLOR, SIZE };
